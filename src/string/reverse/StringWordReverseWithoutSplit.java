c55dde17c011bc14165722df175060fa08226ce8
sawarn17 <ayamshifo@gmail.com>
2024-01-31 17:00:29 +0530

    optimized code is here

--- src/string/reverse/StringWordReverseWithoutSplit.java
+++ src/string/reverse/StringWordReverseWithoutSplit.java
@@ -3,20 +3,34 @@ public class StringWordReverseWithoutSplit {
 	
 	public static void main(String [] args) {
 		String rev = "Hi today is very cold";
-		StringBuilder sb = new StringBuilder();
-		StringBuilder revSb = new StringBuilder();
+//		StringBuilder sb = new StringBuilder();
+//		StringBuilder revSb = new StringBuilder();
 		
-		for(int i=rev.length()-1; i>=0; i--) {
-			if(rev.charAt(i) == ' ') {
-				revSb.append(sb.reverse().append(" "));
-				sb.setLength(0);
-			}else {
-				sb.append(rev.charAt(i));
-			}
-			if (i == 0) {
-	            revSb.append(sb.reverse());
+//		for(int i=rev.length()-1; i>=0; i--) {
+//			if(rev.charAt(i) == ' ') {
+//				revSb.append(sb.reverse().append(" "));
+//				sb.setLength(0);
+//			}else {
+//				sb.append(rev.charAt(i));
+//			}
+//			if (i == 0) {
+//	            revSb.append(sb.reverse());
+//	        }
+//		}
+		
+	    StringBuilder revSb = new StringBuilder();
+	    int wordStart = rev.length();
+
+	    for (int i = rev.length() - 1; i >= 0; i--) {
+	        if (rev.charAt(i) == ' ' || i == 0) {
+	            revSb.append(rev.substring(i, wordStart));
+	            if (i != 0) {
+	                revSb.append(" ");
+	            }
+	            wordStart = i;
 	        }
-		}
+	    }
+		
 		System.out.print(revSb.toString());
 	}
 }
