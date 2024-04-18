38ffae364c565ff1dad92e5041a953a0a3ddcb38
ayams-inventiv <ayarn.sawarn@appinventiv.com>
2024-04-18 09:13:47 +0530

    singleton design pattern break

--- src/designpattern/SingletonDesign.java
+++ src/designpattern/SingletonDesign.java
@@ -1,5 +1,7 @@
 package designpattern;
 
+import java.lang.reflect.Constructor;
+
 class SingletonDemo {
     public static SingletonDemo singleton;
     
@@ -23,5 +25,24 @@ public class SingletonDesign
 	public static void main(String[] args) {
 		System.out.println("HASH CODE ARE "+ SingletonDemo.getSingleTonInstance().hashCode());
 		System.out.println("HASH CODE ARE "+ SingletonDemo.getSingleTonInstance().hashCode());
+
+        //breaking of singleton instance
+        SingletonDemo single1 = SingletonDemo.getSingleTonInstance();
+        SingletonDemo single2 = null;
+
+        Constructor [] constructors = SingletonDemo.class.getDeclaredConstructors();
+        try {
+            for(Constructor cons : constructors){
+                cons.setAccessible(true);
+                single2 = (SingletonDemo) cons.newInstance();
+                break;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("THE instance of first single "+ single1.hashCode());
+        System.out.println("The instance of second instance is "+ single2.hashCode());
+
 	}
 }
