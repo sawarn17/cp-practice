950ea1ad4de1571e0d2676a8e1933be4f5ee174e
ayams-inventiv <ayarn.sawarn@appinventiv.com>
2024-04-20 09:41:51 +0530

    runnbale demo is implemented

---
+++ src/multithreading/RunnableDemo.java
@@ -0,0 +1,18 @@
+package multithreading;
+
+class RunnableEx implements Runnable{
+
+    @Override
+    public void run() {
+        System.out.println("THIS IS RUNNABLE THREAD");
+    }
+    
+}
+
+public class RunnableDemo {
+    public static void main(String[] args) {
+        //here we need to pass the Implementation class of runable
+        Thread run = new Thread(new RunnableEx());
+        run.start();
+    }
+}
