55b7eef0b7432fa2afb2ba2f4e060c88e49ffc39
sawarn17 <ayamshifo@gmail.com>
2024-06-13 21:06:29 +0530

    protypedp imple

---
+++ src/designpattern/prototypedp/ProtoTypeDP.java
@@ -0,0 +1,15 @@
+package designpattern.prototypedp;
+
+public class ProtoTypeDP {
+
+    /*
+     * Implementation of Prototype DP
+     * 
+     * USED WHEN : 
+     *   - Object Creation logic is very complex
+     *   - 
+     */
+    public static void main(String[] args) {
+        
+    }
+}
