628cb233ed78d199a5af9b4da86a3b610f470ab5
sawarn17 <ayamshifo@gmail.com>
2024-07-03 09:59:50 +0530

    code backuped

---
+++ src/linkedlist/easy/MergeTwoSortedList21.java
@@ -0,0 +1,34 @@
+package linkedlist.easy;
+
+/*
+ * This is the representation of linkedlist
+ * Here val is the containing value
+ * 
+ */
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class MergeTwoSortedList21 {
+
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
