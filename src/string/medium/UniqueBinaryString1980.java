6a63a48204b626a141cd306b431438bee5f11405
sawarn17 <ayamshifo@gmail.com>
2025-02-22 20:44:52 +0530

    COde added

---
+++ src/string/medium/UniqueBinaryString1980.java
@@ -0,0 +1,29 @@
+package string.medium;
+
+public class UniqueBinaryString1980 {
+
+    //approach one 
+    /*
+     * Find the decimal value for the number and put into set from minimum value to maximum values
+     * 
+     * 
+     */
+    // public static String findDifferentBinaryString(String[] nums) {
+    //     int n = nums.size();
+        
+    //     StringBuilder result = new StringBuilder();
+        
+    //     for (int i = 0; i < n; i++) {
+    //         char ch = nums.get(i).charAt(i);
+            
+    //         result.append((ch == '0') ? '1' : '0');
+    //     }
+        
+    //     return result.toString();
+    // }
+    
+    // public static void main(String[] args) {
+    //     String [] binaryString = {"111","011","001"};
+    //     System.out.println(findDifferentBinaryString(binaryString));
+    // }
+}
