db1322e48a4eaa0c6685649d3d010bade15107bf
sawarn17 <ayamshifo@gmail.com>
2025-02-26 21:58:27 +0530

    code sync

---
+++ src/string/medium/PalindromicSubstrings647.java
@@ -0,0 +1,13 @@
+package string.medium;
+
+public class PalindromicSubstrings647 {
+
+    public static int countSubstrings(String s) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        String str = "aaa";
+    }
+    
+}
