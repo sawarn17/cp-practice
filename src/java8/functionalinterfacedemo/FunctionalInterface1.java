909dfbf559ca6e70a63044d64810d74e728afe64
sawarn17 <ayamshifo@gmail.com>
2025-03-16 23:59:12 +0530

    functional interface demo is intergarted

--- src/java8/functionalinterfacedemo/FunctionalInterface1.java
+++ src/java8/functionalinterfacedemo/FunctionalInterface1.java
@@ -1,24 +1,16 @@
-package javautiltydemos.functionalinterfaceimpl;
+package java8.functionalinterfacedemo;
 
 import java.util.function.Consumer;
 
 @FunctionalInterface // ensure interface could not have more than one function
-interface  Test {
+interface  FunctionInterface {
     public int getCustomOps(int x, int y);
 }
 
-/*
- * ===============   THIS IS FUNCTIONAL INTERFCE   ===========================
- * It have only one method to be exhabits it have only one abstrct method
- * It is used to Represent the instance of Lambda Experssion
- * It have Runnable, ActionListner , Comparable
- * Calling SAM (Single Abstract Method)
- * 
- * 
- */
-public class FunctionalInterfaceDemo {
+public class FunctionalInterface1 {
+    
 
-    //consumer interfec
+     //consumer interfec
     public static void consumerExample(int x){
          Consumer<Integer> display = a -> System.out.println(a);
          //applied with consumer
@@ -28,9 +20,9 @@ public class FunctionalInterfaceDemo {
 
     public static void main(String[] args) {
         
-        Test add = (x, y) -> x +y;
-        Test sub = (x, y) -> x -y;
-        Test multiply = (x, y) -> x * y;
+        FunctionInterface add = (x, y) -> x +y;
+        FunctionInterface sub = (x, y) -> x -y;
+        FunctionInterface multiply = (x, y) -> x * y;
         
         System.out.println("THE ADD OF NUMBER ARE "+ add.getCustomOps(6, 5));
         System.out.println("THE SUBS OF NUMBER ARE "+ sub.getCustomOps(6, 5));
