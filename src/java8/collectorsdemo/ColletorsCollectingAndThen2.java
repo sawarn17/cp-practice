b1aa39d4b326d5c13ead6e4b4c90b22b589cfeb7
sawarn17 <ayamshifo@gmail.com>
2025-03-28 08:30:53 +0530

    colletors and colleting and then

---
+++ src/java8/collectorsdemo/ColletorsCollectingAndThen2.java
@@ -0,0 +1,35 @@
+package java8.collectorsdemo;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.function.IntPredicate;
+import java.util.function.Predicate;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class ColletorsCollectingAndThen2 {
+
+    // uses of intpredicate
+    private static IntPredicate IS_EVEN_PREDICATE = (x) -> {
+        if (x % 2 == 0)
+            return true;
+        return false;
+    };
+
+    private static Stream<Integer> getListData() {
+        return Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+    }
+
+    public static void demoOfCollectingAndThen() {
+        Set<Integer> unmodifiedableInteger = getListData().filter(IS_EVEN_PREDICATE::test)
+                .collect(Collectors.collectingAndThen(Collectors.toSet(), Collections::<Integer>unmodifiableSet));
+
+        System.out.println("THE RESULT IS " + unmodifiedableInteger);
+    }
+
+    public static void main(String[] args) {
+        demoOfCollectingAndThen();
+    }
+}
