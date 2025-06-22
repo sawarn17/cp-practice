c95fc976410c2dfd8cf1eb7fbee65892ba3aefcc
sawarn17 <ayamshifo@gmail.com>
2025-06-22 22:15:20 +0530

    binary serach backup

---
+++ src/binarysearch/easy/Squart69.java
@@ -0,0 +1,28 @@
+package binarysearch.easy;
+
+public class Squart69 {
+
+    public static int mySqrt(int x) {
+        if (x < 2) {
+            return x;
+        }
+        long start = 1, end = x, ans = 0;
+        while (start <= end) {
+            long mid = start + (end - start) / 2;
+            if (mid * mid == x) {
+                return (int) mid;
+            } else if (mid * mid < x) {
+                start = mid + 1;
+                ans = mid;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return (int) ans;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
