dd2450e854fc104be6f273b13152970e8409ec83
sawarn17 <ayamshifo@gmail.com>
2025-07-29 20:16:58 +0530

    linked list deleted op implemented

--- src/linkedlist/singlelinkedlist/SingleLinkedListBasicCRUD2.java
+++ src/linkedlist/singlelinkedlist/SingleLinkedListBasicCRUD2.java
@@ -1,13 +1,13 @@
 package linkedlist.singlelinkedlist;
 
 public class SingleLinkedListBasicCRUD2 {
-    
-    //createing of singly-linked list
-    private static ListNode1 createListNode1(int [] arr){
+
+    // createing of singly-linked list
+    private static ListNode1 createListNode1(int[] arr) {
         ListNode1 dummy = new ListNode1(0);
         ListNode1 current = dummy;
 
-        for(int i : arr){
+        for (int i : arr) {
             current.next = new ListNode1(i);
             current = current.next;
         }
@@ -15,18 +15,35 @@ public class SingleLinkedListBasicCRUD2 {
         return dummy.next;
     }
 
-    //to show the added linked list
-    private static void displayLinkedList(ListNode1 node){
-        while(node!=null){
-            System.out.println(" "+ node.val);
-            node= node.next;
+    // to show the added linked list
+    private static void displayLinkedList(ListNode1 node) {
+        while (node != null) {
+            System.out.println(" " + node.val);
+            node = node.next;
         }
     }
 
+    private static ListNode1 deletionOfSinglyLinked(ListNode1 node, int valueToBeDeleted) {
+        ListNode1 dummy = new ListNode1(0);
+        dummy.next = node;
+
+        ListNode1 current = dummy;
+
+        while (current.next != null) {
+            if (current.next.val == valueToBeDeleted) {
+                current.next = current.next.next; // skiping the node
+            } else {
+                current = current.next;
+            }
+        }
+
+        return dummy.next; // return the updated head
+    }
+
     public static void main(String[] args) {
-        int [] arr = {1,2,3,4,5,6,7,8,9,10};
-        ListNode1 head= createListNode1(arr);
-        
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        ListNode1 head = createListNode1(arr);
+        deletionOfSinglyLinked(head, 8);
         displayLinkedList(head);
     }
 }
