16323e2289afd58e2ea2774ea82221af6220468c
sawarn17 <ayamshifo@gmail.com>
2025-08-01 08:36:52 +0530

    baisc commits done

---
+++ src/stacks/StackImplUsingArray1.java
@@ -0,0 +1,8 @@
+package stacks;
+
+public class StackImplUsingArray1 {
+    
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/stacks/StackUsingLL2.java
@@ -0,0 +1,8 @@
+package stacks;
+
+public class StackUsingLL2 {
+    
+    public static void main(String[] args) {
+        
+    }
+}
