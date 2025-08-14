3f4ed6aa986a27c7e0789a28147b425536814da7
sawarn17 <ayamshifo@gmail.com>
2025-08-14 08:06:07 +0530

    baisc setup of

---
+++ src/collectiondemo/collectors/BasicCollectorsImpl1.java
@@ -0,0 +1,58 @@
+package collectiondemo.collectors;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class BasicCollectorsImpl1 {
+
+    private static void toListConversionDemo(List<String> details) {
+        // it will display only unique set of eements
+        details.stream().collect(Collectors.toSet()).forEach(System.out::println);
+    }
+
+    // here duplicate keys handles easily
+    private static void mappedToGroupingBy(List<String> details) {
+        details.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet()
+                .forEach(System.out::println);
+    }
+
+    // make partion based on the predicated passed in it
+    private static void partitionByDemo(List<String> detaList) {
+        Map<Boolean, List<String>> result = detaList.stream()
+                .collect(Collectors.partitioningBy(data -> data.length() % 2 == 0));
+        System.out.println(result);
+        // OP: {false=[Ayarn, Vinod, Vimal], true=[Amit, Vishal, Vishal, Rama]}
+    }
+
+    //Collector.of allows the creation of a custom collector. 
+    //This is especially useful for more specialized reductions that are not covered by the standard collectors.
+    private static void demoCustomCollectors() {
+        Collector<String, StringBuilder, String> joiningCollector = Collector.of(
+                StringBuilder::new,
+                StringBuilder::append,
+                StringBuilder::append,
+                StringBuilder::toString);
+
+        String result = Stream.of("Aya", "RN ", "SAWARN").collect(joiningCollector);
+        System.out.println(result);
+    }
+
+    /*
+     * Collectors allow us to transform and aggregate stream elements in diverse and
+     * complex ways.
+     */
+    public static void main(String[] args) {
+        List<String> details = Arrays.asList("Ayarn", "Amit", "Vishal", "Vinod", "Vimal", "Vishal", "Rama");
+
+        toListConversionDemo(details);
+        mappedToGroupingBy(details);
+        partitionByDemo(details);
+        demoCustomCollectors();
+    }
+}
---
+++ src/collectiondemo/listinterface/ArrayListDemo2.java
@@ -0,0 +1,5 @@
+package collectiondemo.listinterface;
+
+public class ArrayListDemo2 {
+    
+}
