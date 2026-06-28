91aba534fe5bb95d5251131f1ffd1d2b60b9dd90
sawarn17 <ayamshifo@gmail.com>
2026-06-28 07:46:11 +0530

    basic-setup

--- src/stacks/StackImplUsingArray1.java
+++
@@ -1,8 +0,0 @@
-package stacks;
-
-public class StackImplUsingArray1 {
-    
-    public static void main(String[] args) {
-        
-    }
-}
--- src/stacks/StackUsingLL2.java
+++
@@ -1,8 +0,0 @@
-package stacks;
-
-public class StackUsingLL2 {
-    
-    public static void main(String[] args) {
-        
-    }
-}
